1de126373fc9eab6fbab6939467e7b16cb77f1f6
chojui88 <chojui88@gmail.com>
2026-03-26 14:35:29 +0900

    거래내역 출력방식 변경

--- src/main/java/capstone/payment_generator/PaymentService.java
+++ src/main/java/capstone/payment_generator/PaymentService.java
@@ -1,16 +1,42 @@
 package capstone.payment_generator;
 
+import jakarta.websocket.SendResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j //로그 기록
 public class PaymentService {
     private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String TOPIC = "payment-events";
 
     public void send(PaymentEvent event){
-        kafkaTemplate.send("payment-logs",event);
+        kafkaTemplate.send(TOPIC, event.getTransactionId(), event)
+                        .whenComplete((result, ex) -> {
+                            if (ex == null) {
+                                log.info("[결제발생] 거래ID: {} | 계좌: {} | 유저: {} | 금액: {}원 | 시간: {} | IP: {} | 기기: {} | Partition: {} | Offset: {}",
+                                        event.getTransactionId(),
+                                        event.getAccountNo(),
+                                        event.getUserId(),
+                                        String.format("%,d", event.getAmount()),
+                                        event.getTimestamp(),
+                                        event.getIpAddress(),
+                                        event.getDeviceId(),
+                                        result.getRecordMetadata().partition(),
+                                        result.getRecordMetadata().offset()
+                                        );
+                            }else {
+                                log.error("❌ [Kafka Failed] TransactionId: {} | Reason: {}",
+                                        event.getTransactionId(), ex.getMessage(), ex);
+
+                            }
+                        });
+
     }
 
 }
